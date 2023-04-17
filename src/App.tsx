6b100c1bd38e1e5e2425003e3f801772724994fc
정수진 <sssjsjj@gmail.com>
2023-04-17 15:07:35 +0900

    refactor: RandomResult 관련 코드 App 파일과 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,13 @@
 import styled from 'styled-components';
 import GlobalStyle from './global-style'
-import ContentSection from './components/ContentSection';
-import InputBadges from './components/InputBadges';
-import Input from './elements/Input';
-import useInputs from './hooks/useInputs';
-import { SyntheticEvent, useCallback, useState } from 'react';
-import { getObjFromKeyArr, getDataListType } from './utils';
-
-interface Forms {
-  [key: string]: string
-}
-
-interface AddedDataList {
-  [key: string]: string[]
-}
+import RandomResult from './pages/RandomResult';
 
 function App() {
-  const dataTypes = ['memberName', 'caseName']
-  const dataListTypes = dataTypes.map((dataType) => getDataListType(dataType))
-  const [forms, onChange, reset, setForms] = useInputs<Forms>(
-    getObjFromKeyArr<string>(dataTypes, '')
-  )
-  const [addedDataList, setAddedDataList] = useState<AddedDataList>(
-    getObjFromKeyArr<string[]>(dataListTypes, [])
-  )
-
-  const onSubmit = useCallback((e: SyntheticEvent, dataType: string) => {
-    e.preventDefault()
-    const dataListType = getDataListType(dataType)
-    const newData = forms[dataType]
-    setAddedDataList((prev) => ({
-      ...prev,
-      [dataListType]: [...prev[dataListType], newData]
-    }))
-    // reset form
-    setForms((prev) => ({...prev, [dataType]: ''}))
-  }, [forms, addedDataList])
-
-  const getInputComp = useCallback((dataType: string) => (
-    <Input
-      name={dataType}
-      value={forms[dataType]}
-      onChange={onChange}
-    />
-  ), [forms, onChange])
-
   return (
       <>
         <GlobalStyle />
         <AppStyled>
-          {dataTypes.map((dataType: string) => (
-            <ContentSection
-              key={dataType}
-              title={dataType}
-            >
-              <InputBadges
-                InputComp={getInputComp(dataType)}
-                dataType={dataType}
-                dataList={addedDataList[getDataListType(dataType)]}
-                onSubmit={onSubmit}
-              />
-            </ContentSection>
-          ))}
+          <RandomResult />
         </AppStyled>
       </>
   );
