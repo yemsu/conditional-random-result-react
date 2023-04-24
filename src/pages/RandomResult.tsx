be3f478c170e717cf4e8dc614111fdeb756d31ec
정수진 <sssjsjj@gmail.com>
2023-04-24 16:06:55 +0900

    feat: 추가된 예외 노출 및 저장 기능 추가

--- src/pages/RandomResult.tsx
+++ src/pages/RandomResult.tsx
@@ -4,12 +4,15 @@ import InputBadges from '../components/InputBadges';
 import Input from '../elements/Input';
 import Button from "../elements/Button";
 import useInputs from '../hooks/useInputs';
-import { SyntheticEvent, useCallback } from 'react';
+import { SyntheticEvent, useCallback, useState } from 'react';
 import { getObjFromKeyArr } from '../utils';
 import OptionButtons from "../elements/OptionButtons";
 import useOptionButtons from "../hooks/useOptionButtons";
 import useInputBadges from "../hooks/useInputBadges";
-import { Forms } from "../types/common";
+import { Forms, OptionButtonsState } from "../types/common";
+import List from "../elements/List";
+import useLocalStorage from "../hooks/useLocalStorage";
+import { STORAGE_NAME } from "../constants/clientStorage";
 
 function RandomResult() {
   const dataTypes = ['memberName', 'caseName']
@@ -20,9 +23,11 @@ function RandomResult() {
     getObjFromKeyArr<string[]>(dataTypes, []),
     setForms
   )
-  const [exceptions, onSelectException, resetExceptions] = useOptionButtons(
+  const [selectedExceptions, onSelectException, resetSelectedExceptions] = useOptionButtons(
     getObjFromKeyArr<string[]>(dataTypes, [])
   )
+  const [exceptions, setExceptions] = useState<OptionButtonsState[]>([])
+  const [saveExceptionData, deleteSavedExceptionData ] = useLocalStorage<OptionButtonsState[]>(STORAGE_NAME.SELECTED_EXCEPTIONS, setExceptions)
 
   const onSubmit = useCallback((e: SyntheticEvent, dataType: string) => {
     e.preventDefault()
@@ -39,7 +44,18 @@ function RandomResult() {
 
   const onClickResetInputData = useCallback(() => {
     resetInputData()
-    resetExceptions()
+    resetSelectedExceptions()
+  }, [])
+
+  const addException = useCallback(() => {
+    setExceptions(prev => [...prev, selectedExceptions])
+    saveExceptionData([...exceptions, selectedExceptions])
+    resetSelectedExceptions()
+  }, [selectedExceptions])
+
+  const resetExceptions = useCallback(() => {
+    setExceptions([])
+    resetSelectedExceptions()
   }, [])
 
   return (
@@ -69,14 +85,27 @@ function RandomResult() {
                 key={`${dataType}-exceptions`}
                 dataType={dataType}
                 dataList={inputDataList[dataType]}
-                selectedList={exceptions[dataType]}
+                selectedList={selectedExceptions[dataType]}
                 onSelect={onSelectException}
               />
             </>
           ))}
         </ContentSection>
       }
+      <Button onClick={addException}>예외 추가하기</Button>
       <Button onClick={resetExceptions}>resetExceptions</Button>
+      
+      <h3>추가된 예외</h3>
+      <List
+        dataList={exceptions}
+      >
+        {({memberName, caseName}) => (
+          <>            
+            멤버: {memberName}
+            예외: {caseName}
+          </>
+        )}        
+      </List>
     </div>
   )
 }
