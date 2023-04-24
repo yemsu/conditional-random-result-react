088aa5fc8f88940994a1b177b8e41405a599c603
정수진 <sssjsjj@gmail.com>
2023-04-24 16:04:17 +0900

    fix: List 컴포넌트 다양한 데이터 리스트 타입 전달받을 수 있도록 수정

--- src/elements/List.tsx
+++ src/elements/List.tsx
@@ -1,13 +1,13 @@
 import { ElementType, ReactNode } from "react"
 import styled, { css } from "styled-components"
 
-interface ListProps {
-  dataList: string[]
-  children: (data: string) => ReactNode
+interface ListProps<DataType> {
+  dataList: DataType[]
+  children: (data: DataType) => ReactNode
   direction?: 'column' | 'row'
 }
 
-function List(props: ListProps) {
+function List<DataType>(props: ListProps<DataType>) {
   const {
     dataList,
     children,
@@ -16,7 +16,7 @@ function List(props: ListProps) {
 
   return (
     <ListStyled theme={styleThemeMap[direction]}>
-      {dataList.map((data: string, i) => (
+      {dataList.map((data: DataType, i) => (
         <ListItemStyled key={i}>
           { children(data) }
         </ListItemStyled>
